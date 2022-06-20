452518172e1da5730d8940e0f61ed6080387e73c
Kimberley Tham <kimbertham@gmail.com>
2022-06-21 00:38:27 +0100

    d

--- index.ts
+++ index.ts
@@ -32,7 +32,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../frontend')))
 
   app.get('*',(req: Request, res: Response) => {
-    res.sendFile(path.resolve(__dirname,'frontend','build','index.html'))
+    res.sendFile(path.resolve(__dirname,'../frontend'))
   })
 }
 
