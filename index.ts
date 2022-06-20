f3470daf09ad520d375b1c144c2e66a665ea7ff8
Kimberley Tham <kimbertham@gmail.com>
2022-06-20 23:42:29 +0100

    d

--- index.ts
+++ index.ts
@@ -29,11 +29,13 @@ app.use('/api', router)
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname + 'frontend/build')))
+
+  app.get('*', (req: any, res: { sendFile: (arg0: string) => void }) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html' ))
+  })
 }
 
-app.get('*', (req: any, res: { sendFile: (arg0: string) => void }) => {
-  res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html' ))
-})
+
 
 app.listen(PORT, () => {
   console.log('listening on port 8000')
