57a10fc2348365a8d372d79ed0ae929731cba645
Kimberley Tham <kimbertham@gmail.com>
2022-06-20 19:58:39 +0100

    d

--- index.ts
+++ index.ts
@@ -25,7 +25,7 @@ db.once('open', function () {
 app.use(express.static(__dirname + '/dist/'))
 
 app.get('*', function (_request: any, response: { sendFile: (arg0: any) => void }) {
-  response.sendFile(path.resolve(__dirname, '../frontend/my-app/build', 'index.html'))
+  response.sendFile(path.resolve(__dirname, '/frontend/my-app/build', 'index.html'))
 })
 
 app.use(json())
